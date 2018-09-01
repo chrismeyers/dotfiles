6cf99d0a97eeaa3d98d2e132e78c0c4a4b37cd75
Chris Meyers <cm.02.93@gmail.com>
2018-09-01 15:08:21 -0400

    converted path construction from f-strings to os.path.join().

--- backupdots.py
+++ backupdots.py
@@ -26,18 +26,18 @@ def perform_backup():
     # Copies files from original location to dotfiles/...
     file_num = 1
     for file in _backup_data:
-        orig_file = f'{_backup_data[file][0]}/{file}'
-        backup_file = f'{_backup_data[file][1]}/{file}'
+        orig_file = os.path.join(_backup_data[file][0], file)
+        backup_file = os.path.join(_backup_data[file][1], file)
 
         if not os.path.exists(backup_file.replace("'", "")) and not os.path.islink(orig_file):
             if not os.path.exists(_backup_data[file][1]):
                 os.makedirs(_backup_data[file][1], mode=0o755)
             if not os.path.isdir(orig_file):
                 backup_type = "file"
-                shutil.copy(f'{_backup_data[file][0]}/{file}', _backup_data[file][1])
+                shutil.copy(os.path.join(_backup_data[file][0], file), _backup_data[file][1])
             else:
                 backup_type = "directory"
-                shutil.copytree(f'{_backup_data[file][0]}/{file}', f'{_backup_data[file][1]}/{file}')
+                shutil.copytree(os.path.join(_backup_data[file][0], file), os.path.join(_backup_data[file][1], file))
             print(f'{str(file_num).rjust(3)} Copied {backup_type}: {file} to {_backup_data[file][1]}')
             file_num += 1
 
@@ -46,12 +46,12 @@ def perform_backup():
 
     if (_args.platform).lower() == "mac":
         print('Dumping installed homebrew packages...')
-        os.system(f'{_backup_dir_root}/Mac/Homebrew/dump.sh')
+        os.system(os.path.join(_backup_dir_root, 'Mac/Homebrew/dump.sh'))
         print('...brew bundle dump complete!')
 
     if (_args.platform).lower() == "linux":
         print('Dumping GNOME Terminal default profile...')
-        os.system(f'{_backup_dir_root}/Linux/GNOMETerminal/dump.sh')
+        os.system(os.path.join(_backup_dir_root, 'Linux/GNOMETerminal/dump.sh'))
         print('...profile dump complete!')
 
 
@@ -59,15 +59,15 @@ def perform_restore():
     # Symlinks files from dotfiles/... to original location.
     file_num = 1
     for file in _backup_data:
-        orig_file = f'{_backup_data[file][0]}/{file}'
-        backup_file = f'{_backup_data[file][1]}/{file}'
+        orig_file = os.path.join(_backup_data[file][0], file)
+        backup_file = os.path.join(_backup_data[file][1], file)
 
         # Assume that the program isn't installed or the configuration file is
         # not needed if the original path doesn't exist.
         if os.path.exists(_backup_data[file][0]):
             # Make a backup of the file before creating a symlink.
             if os.path.exists(orig_file) and not os.path.islink(orig_file):
-                shutil.move(orig_file, f'{orig_file}{_backup_file_ext}')
+                shutil.move(orig_file, f'{orig_file}.{_backup_file_ext}')
             if not os.path.exists(orig_file):
                 os.symlink(backup_file, orig_file)
                 print(f'{str(file_num).rjust(3)} Linked {"directory" if os.path.isdir(backup_file) else "file"}: {file} to {_backup_data[file][0]}')
@@ -80,10 +80,10 @@ def perform_restore():
 
 
 def perform_cleanup():
-    # Removes all *{_backup_file_ext} files generated from perform_restore().
+    # Removes all *.{_backup_file_ext} files generated from perform_restore().
     file_num = 1
     for file in _backup_data:
-        current_file = f'{_backup_data[file][0]}/{file}{_backup_file_ext}'
+        current_file = os.path.join(_backup_data[file][0], f'{file}.{_backup_file_ext}')
 
         if os.path.exists(current_file):
             if not os.path.isdir(current_file):
@@ -103,8 +103,8 @@ def perform_unlink():
     # Removes all symlinks for the given platform.
     file_num = 1
     for file in _backup_data:
-        is_dir = os.path.isdir(f'{_backup_data[file][1]}/{file}')
-        current_file = f'{_backup_data[file][0]}/{file}'
+        is_dir = os.path.isdir(os.path.join(_backup_data[file][1], file))
+        current_file = os.path.join(_backup_data[file][0], file)
 
         if os.path.exists(current_file):
             os.unlink(current_file)
@@ -125,19 +125,19 @@ def sanitized_full_path(dir_location, file_name):
     if file_name.startswith("/"):
         sanitized_file_name = file_name[1:]
 
-    return f'{sanitized_dir_location}/{sanitized_file_name}'
+    return os.path.join(sanitized_dir_location, sanitized_file_name)
 
 
 if __name__ == "__main__":
     _backup_dir_root = os.path.dirname(os.path.abspath(__file__))
     _backup_config_file = sanitized_full_path(_backup_dir_root, 'backupdots.json')
-    _backup_file_ext = ".orig"
+    _backup_file_ext = "orig"
 
     arg_parser = argparse.ArgumentParser(description='Backup or restore configuration files.')
     arg_parser.add_argument('-p', '--platform', help='specifies which set of files to use (Mac, Linux, Windows)', required=True)
     arg_parser.add_argument('-b', '--backup', help='perform a backup based on files in backupdots.json', action='store_true')
     arg_parser.add_argument('-r', '--restore', help='perform a restore based on files in backupdots.json', action='store_true')
-    arg_parser.add_argument('-c', '--cleanup', help=f'removes *{_backup_file_ext} files', action='store_true')
+    arg_parser.add_argument('-c', '--cleanup', help=f'removes *.{_backup_file_ext} files', action='store_true')
     arg_parser.add_argument('-u', '--unlink', help=f'removes all symlinks for the given platform', action='store_true')
     _args = arg_parser.parse_args()
 
