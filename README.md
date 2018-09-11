9596d5c8a01efdc192b5dba201634c5e191dd164
Chris Meyers <cm.02.93@gmail.com>
2018-09-10 22:27:27 -0400

    added the --tree flag to backupdots.py.

--- README.md
+++ README.md
@@ -3,3 +3,110 @@ My personal configuration files.
 
 These files are backed up and can be restored using backupdots.py in conjunction
 with backupdots.json.
+
+<!-- BEGIN TREE -->
+```
+.
+├── Common
+│   ├── misc
+│   │   └── .astylerc
+│   ├── neovim
+│   │   └── nvim
+│   │       └── init.vim
+│   ├── SublimeText
+│   │   └── UserSettings
+│   │       ├── Package Control.sublime-settings
+│   │       └── Python.sublime-settings
+│   └── vim
+│       ├── .gvimrc
+│       └── .vimrc
+├── Linux
+│   ├── bash
+│   │   ├── .aliases
+│   │   └── .bashrc
+│   ├── compton
+│   │   └── compton.conf
+│   ├── fontconfig
+│   │   └── conf.d
+│   │       └── 10-symbols.conf
+│   ├── Git
+│   │   ├── .gitconfig
+│   │   └── .gitignore_global
+│   ├── GNOMETerminal
+│   │   ├── Default.dconf
+│   │   ├── dump.sh
+│   │   └── load.sh
+│   ├── i3
+│   │   └── config
+│   ├── i3status
+│   │   └── config
+│   ├── polybar
+│   │   ├── config
+│   │   └── launch.sh
+│   ├── rofi
+│   │   ├── config
+│   │   ├── material.rasi
+│   │   └── sidetab.rasi
+│   ├── SublimeText
+│   │   └── UserSettings
+│   │       └── Preferences.sublime-settings
+│   ├── systemd
+│   │   └── i3lock.service
+│   └── Wallpapers
+│       ├── moon_mower.jpg
+│       ├── moon_mower.png
+│       ├── ruby_beach.jpg
+│       └── ruby_beach.png
+├── Mac
+│   ├── bash
+│   │   ├── .aliases
+│   │   ├── .bash_profile
+│   │   ├── .bashrc
+│   │   └── MCG
+│   │       ├── .aliases
+│   │       └── .bashrc
+│   ├── Git
+│   │   ├── .gitconfig
+│   │   ├── .gitignore_global
+│   │   └── MCG
+│   │       └── .gitconfig
+│   ├── Homebrew
+│   │   ├── Brewfile
+│   │   ├── dump.sh
+│   │   └── restore.sh
+│   ├── HostFiles
+│   │   ├── Homebrew
+│   │   │   └── usr
+│   │   │       └── local
+│   │   │           └── etc
+│   │   │               └── apache2
+│   │   │                   └── 2.4
+│   │   │                       ├── extra
+│   │   │                       │   └── httpd-vhosts-homebrew.conf
+│   │   │                       └── httpd.conf
+│   │   └── MAMP
+│   │       ├── httpd-vhosts.conf
+│   │       └── templates
+│   │           ├── httpd_MAMP_PRO_TEMPLATE.conf
+│   │           ├── main_MAMP_PRO_TEMPLATE.cf
+│   │           ├── my56_MAMP_PRO_TEMPLATE.cnf
+│   │           ├── nginx_MAMP_PRO_TEMPLATE.conf
+│   │           └── php7.1.8_MAMP_PRO_TEMPLATE.ini
+│   ├── iTermProfiles
+│   │   └── Default
+│   │       ├── com.googlecode.iterm2.plist
+│   │       └── iterm2_default.itermcolors
+│   └── SublimeText
+│       └── UserSettings
+│           ├── Preferences.sublime-settings
+│           └── Python3.sublime-build
+└── Windows
+    ├── Git
+    │   └── .gitconfig
+    └── SublimeText
+        └── UserSettings
+            └── Preferences.sublime-settings
+
+46 directories, 53 files
+```
+<!-- END TREE -->
--- backupdots.py
+++ backupdots.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-usage: backupdots.py [-h] -p PLATFORM [-b] [-r] [-c] [-u]
+usage: backupdots.py [-h] -p PLATFORM [-b] [-r] [-c] [-u] [-t MODE]
 
 Backup or restore configuration files.
 
@@ -14,6 +14,9 @@ optional arguments:
   -r, --restore         perform a restore based on files in backupdots.json
   -c, --cleanup         removes *.orig files
   -u, --unlink          removes all symlinks for the given platform
+  -t MODE, --tree MODE  generates a directory tree and prints the output to
+                        stdout or injects the output into README.md (print,
+                        inject)
 '''
 
 import os
@@ -21,6 +24,7 @@ import sys
 import json
 import argparse
 import shutil
+import subprocess
 
 def perform_backup():
     # Copies files from original location to dotfiles/...
@@ -131,6 +135,70 @@ def perform_unlink():
         print("Nothing to unlink...")
 
 
+def perform_tree():
+    tree_mode = (_args.tree).lower()
+
+    if tree_mode not in _tree_modes:
+        print(f'ERROR: Invalid --tree mode. Supported modes: {", ".join(_tree_modes)}.')
+        sys.exit(1)
+
+    tree = generate_tree()
+    if tree_mode == 'print':
+        print(tree)
+    elif tree_mode == 'inject':
+        inject_tree(tree)
+
+
+def generate_tree():
+    p = subprocess.Popen("tree -a -I '.git|.gitignore|backupdots.*|README.md'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+
+    out = out.decode('utf-8')
+    err = err.decode('utf-8')
+
+    if err != '':
+        print(f'ERROR: {err}')
+        sys.exit(1)
+
+    return out
+
+
+def inject_tree(tree):
+    readme_path = os.path.join(_path_to_this_script, 'README.md')
+
+    if not os.path.exists(readme_path):
+        print(f'ERROR: {readme_path} does not exist!')
+        sys.exit(1)
+
+    with open(readme_path, 'r') as f_in, open(f'{readme_path}.tmp', 'w+') as f_out:
+        readme_lines = f_in.readlines()
+        begin_index = -1
+        end_index = -1
+
+        for i, line in enumerate(readme_lines):
+            if line.strip() == '<!-- BEGIN TREE -->':
+                begin_index = i
+            elif line.strip() == '<!-- END TREE -->':
+                end_index = i
+
+        if begin_index > 0 and end_index > 0:
+            # Remove all the lines between BEGIN and END tags, then replace
+            # with the new tree.
+            del readme_lines[begin_index + 1:end_index]
+            readme_lines.insert(begin_index + 1, f'```\n{tree}```\n')
+
+            # TODO: Can this be done with one file (README.md)?
+            for line in readme_lines:
+                f_out.write(line)
+            os.remove(readme_path)
+            os.rename(f'{readme_path}.tmp', readme_path)
+
+            print('Updated directory tree in README.md...')
+        else:
+            print('ERROR: BEGIN and END tags are likely missing from README.md')
+            sys.exit(1)
+
+
 def sanitized_full_path(dir_location, file_name):
     sanitized_dir_location = dir_location
     sanitized_file_name = file_name
@@ -161,21 +229,28 @@ if __name__ == "__main__":
     _backup_dir_root = os.path.dirname(os.path.abspath(__file__))
     _backup_config_file = sanitized_full_path(_backup_dir_root, 'backupdots.json')
     _backup_file_ext = "orig"
+    _tree_modes = ['print', 'inject']
+    _path_to_this_script = os.path.dirname(os.path.abspath(__file__))
+
+    with open(_backup_config_file) as f:
+        _all_backup_data = json.load(f)
+
+    _platforms = list(_all_backup_data.keys())
 
     arg_parser = argparse.ArgumentParser(description='Backup or restore configuration files.')
-    arg_parser.add_argument('-p', '--platform', help='specifies which set of files to use (Mac, Linux, Windows)', required=True)
+    arg_parser.add_argument('-p', '--platform', help=f'specifies which set of files to use ({", ".join(_platforms)})', required=True)
     arg_parser.add_argument('-b', '--backup', help='perform a backup based on files in backupdots.json', action='store_true')
     arg_parser.add_argument('-r', '--restore', help='perform a restore based on files in backupdots.json', action='store_true')
     arg_parser.add_argument('-c', '--cleanup', help=f'removes *.{_backup_file_ext} files', action='store_true')
     arg_parser.add_argument('-u', '--unlink', help=f'removes all symlinks for the given platform', action='store_true')
+    arg_parser.add_argument('-t', '--tree', help=f'generates a directory tree and prints the output to stdout or injects the output into README.md ({", ".join(_tree_modes)})', metavar='MODE')
     _args = arg_parser.parse_args()
 
     if not os.path.exists(_backup_config_file):
         print(f'ERROR: Configuration file "{_backup_config_file}" does not exist.')
         sys.exit(1)
 
-    with open(_backup_config_file) as f:
-        _backup_data = json.load(f)[(_args.platform).capitalize()]
+    _backup_data = _all_backup_data[(_args.platform).capitalize()]
 
     if _args.backup:
         perform_backup()
@@ -185,5 +260,7 @@ if __name__ == "__main__":
         perform_cleanup()
     elif _args.unlink:
         perform_unlink()
+    elif _args.tree:
+        perform_tree()
     else:
         perform_backup()
