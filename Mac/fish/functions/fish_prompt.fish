5d558e67846f227bdb1bbc28210ead09c0609a33
Chris Meyers <cm.02.93@gmail.com>
2019-03-30 19:34:28 -0600

    added git status indicators to fish prompt.

--- Mac/fish/functions/fish_prompt.fish
+++ Mac/fish/functions/fish_prompt.fish
@@ -9,9 +9,24 @@ function fish_prompt
 
   printf '[%s]' (prompt_pwd)
 
-  set_color green
-  printf ' %s' (git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')
-  set_color normal
+  if test (git rev-parse --is-inside-work-tree ^ /dev/null)
+    set git_branch (git --no-pager branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+
+    if [ -z (git status --short) ]
+      set git_status "✓"
+    else
+      set git_status_color "red"
+      set git_status "✗"
+    end
+
+    set_color green
+    printf ' (%s ' $git_branch
+    set_color $git_status_color
+    printf '%s' $git_status
+    set_color green
+    printf ')'
+    set_color normal
+  end
 
   # Line 2
   echo
