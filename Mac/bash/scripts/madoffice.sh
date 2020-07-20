ee2efbae970d0d40d2f6a66bfafd5f71399874fa
Chris Meyers <chris.meyers@madwiremedia.com>
2020-07-20 15:08:24 -0600

    Moved common build and run code into a function.

--- Mac/bash/scripts/madoffice.sh
+++ Mac/bash/scripts/madoffice.sh
@@ -3,39 +3,38 @@
 # Starts the services required for Madoffice development
 # usage: ./madoffice.sh [build]
 
+BLUE_BOLD="\033[1;34m"
+NORMAL="\033[0m"
 [[ $1 == "build" ]] && build=true || build=false
 
-echo "Starting Madoffice Legacy..."
+function go {
+  local tag=$1
+  local build_script=$2
+  local run_script=$3
+
+  if [[ $build == true ]]; then
+    printf "${BLUE_BOLD}Building $tag...${NORMAL}\n"
+    ./$build_script
+  fi
+
+  printf "${BLUE_BOLD}Starting $tag...${NORMAL}\n"
+  ./$run_script
+}
+
 (
   cd ~/.captain/headproxy/madoffice-legacy-development/docker/legacy/development
-
-  if [[ $build == true ]]; then
-    ./build.development.sh
-  fi
-
-  ./run.development.sh
+  go "Madoffice Legacy" build.development.sh run.development.sh
 )
 
-echo "Starting Madoffice API..."
 (
   cd ~/.captain/headproxy/madoffice-api-development/docker/api
-
-  if [[ $build == true ]]; then
-    ./build.development.sh
-  fi
-
-  ./run.development.sh
+  go "Madoffice API" build.development.sh run.development.sh
 )
 
-echo "Starting Billing API..."
 (
   cd ~/.captain/headproxy/billing-api-development/docker/api
-
-  if [[ $build == true ]]; then
-    ./build.development.sh
-  fi
-
-  ./run.development.sh
+  go "Billing API" build.development.sh run.development.sh
 )
 
+printf "${BLUE_BOLD}Here's what docker has to say:${NORMAL}\n"
 docker ps
