7d58c5fa906d1edff277dd889af197ee6e2a0c9f
Chris Meyers <chris.meyers@madwiremedia.com>
2020-07-20 14:17:47 -0600

    Added optional `build` to Madoffice startup script.

--- Mac/bash/scripts/madoffice.sh
+++ Mac/bash/scripts/madoffice.sh
@@ -1,21 +1,39 @@
 #!/usr/bin/env bash
 
+# Starts the services required for Madoffice development
+# usage: ./madoffice.sh [build]
+
 echo "Starting Madoffice Legacy..."
 (
-  cd ~/.captain/headproxy/madoffice-legacy-development/docker/legacy/development \
-    && ./run.development.sh
+  cd ~/.captain/headproxy/madoffice-legacy-development/docker/legacy/development
+
+  if [[ $1 == 'build' ]]; then
+    ./build.development.sh
+  fi
+
+  ./run.development.sh
 )
 
 echo "Starting Madoffice API..."
 (
-  cd ~/.captain/headproxy/madoffice-api-development/docker/api \
-    && ./run.development.sh
+  cd ~/.captain/headproxy/madoffice-api-development/docker/api
+
+  if [[ $1 == 'build' ]]; then
+    ./build.development.sh
+  fi
+
+  ./run.development.sh
 )
 
 echo "Starting Billing API..."
 (
-  cd ~/.captain/headproxy/billing-api-development/docker/api \
-    && ./run.development.sh
+  cd ~/.captain/headproxy/billing-api-development/docker/api
+
+  if [[ $1 == 'build' ]]; then
+    ./build.development.sh
+  fi
+
+  ./run.development.sh
 )
 
 docker ps
